ad71b27e2d9430939858f74df6eb464477333544
Karansidds <=>
2019-09-22 18:21:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let dismiss = document.querySelector('#dismiss');
+  let full_nav = document.querySelector('.fullscreen-nav');
+  let nav_btn = document.querySelector('.nav-menu');
+  let navbar = document.querySelector('.navbar');
+
+  nav_btn.addEventListener('click', () => {
+    nav_btn.classList.add('d-none');
+    full_nav.classList.remove('d-none');
+    navbar.classList.add('d-none');
+  })
+
+  dismiss.addEventListener('click', () => {
+    full_nav.classList.add('d-none');
+    navbar.classList.remove('d-none');
+    nav_btn.classList.remove('d-none');
+  })
+})
