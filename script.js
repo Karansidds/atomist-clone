28cd389f33c5444690eeb28d4657a44b34892d0b
Karansidds <=>
2019-10-04 18:29:53 +0530

    Added dark mode to the website along with some other changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // NAVBAR
   let dismiss = document.querySelector('#dismiss');
   let full_nav = document.querySelector('.fullscreen-nav');
   let nav_btn = document.querySelector('.nav-menu');
@@ -17,5 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
     navbar.classList.remove('d-none');
     nav_btn.classList.remove('d-none');
     tick.play();
+  })
+
+  // DARK MODE
+  let dark_icon = document.querySelector('.dark-icon');
+  let dark_icon_img = document.querySelector('.dark_icon_img');
+  let body = document.querySelector('body');
+
+  dark_icon.addEventListener('click', () => {
+    if (dark_icon_img.src.includes('moon')) {
+      dark_icon_img.src = "./images/sun.png";
+      body.classList.add('dark-mode');
+    } else if (dark_icon_img.src.includes('sun')) {
+      dark_icon_img.src = "./images/moon.png";
+      body.classList.remove('dark-mode');
+    }
   })
 })
